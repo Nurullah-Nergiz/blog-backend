c505b7553dc49a5b0298e16eb33b1175d3e0a90d
Nurullah-Nergiz <nurullah.devs@gmail.com>
2024-02-11 17:59:29 +0300

    Update Post Schema

--- src/schema/post.js
+++ src/schema/post.js
@@ -8,7 +8,7 @@ export default model(
    new Schema(
       {
          title: { type: String, required: true },
-         slug: { type: String, required: true },
+         slug: { type: String, default:"",            unique: true, },
          description: { type: String, required: true },
          thumbnail: { type: String },
          content: { type: String, required: true },
