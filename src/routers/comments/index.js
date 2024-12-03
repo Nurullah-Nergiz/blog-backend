c3de59aa09e15ff21ca20f9bffa78e3dd5f79262
Nurullah NERGİZ <nurullah.devs@gmail.com>
2024-12-03 17:30:08 +0300

    Add Comments Route: implement GET and POST endpoints for managing comments with user and post associations

--- src/routers/comments/index.js
+++ src/routers/comments/index.js
@@ -3,15 +3,27 @@ import commentSchema from "../../schema/comment.js";
 // import {} from '../../validations/commnet.js';
 
 const route = () => {
+   /** @type {import('express').Router} */
    const app = new Router();
 
-   app.get("/", (req, res) => {
-      res.status(200).json("commnet Page Get Method");
+   app.get("/:id", (req, res) => {
+      commentSchema
+         .find({ postId: req.params.id })
+         .populate("userId")
+         .populate("replies")
+         .then((comments) => {
+            res.status(200).json(comments);
+         })
+         .catch((err) => {
+            res.status(500).json(err);
+         });
+      
+      // res.status(200).json("commnet Page Get Method");
    });
 
    app.post("/:id", (req, res) => {
       console.log(req.body);
-      const comments = new commentSchema(req.body);
+      const comments = new commentSchema({ userId: req.user?._id, postId: req.params.id, ...req.body });
       comments
          .save()
          .then((user) => {
@@ -19,8 +31,10 @@ const route = () => {
             res.status(201).json(user);
          })
          .catch((err) => {
+            console.log(req.user);
             res.status(500).json([req.body, err]);
          });
+      // res.status(200).json("user");
    });
 
    return app;
@@ -28,6 +42,6 @@ const route = () => {
 
 export default {
    prefix: "/comments/",
-   checkAuth: true,
+   isLogin: true,
    route,
 };
--- src/schema/comment.js
+++ src/schema/comment.js
@@ -2,10 +2,15 @@ import { Schema, Types, model } from "mongoose";
 
 export default model(
    "Comments",
-   new Schema({
-      text: { type: String, required: true },
-      author: { type: Types.ObjectId, ref: "User", required: true },
-      blog: { type: Types.ObjectId, ref: "blog" },
-      replies: { type: Types.ObjectId, ref: "comment", default: "" },
-   })
+   new Schema(
+      {
+         text: { type: String, required: true, required: true },
+         userId: { type: Types.ObjectId, ref: "User", required: true },
+         postId: { type: Types.ObjectId, ref: "Post", required: true },
+         replies: { type: Types.ObjectId, ref: "Comment" },
+      },
+      {
+         timestamps: true,
+      }
+   )
 );
