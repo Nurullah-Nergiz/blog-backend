8dfdbc8c832a7d988bf8a01861d99d4313681b99
Nurullah NERGİZ <nurullah.devs@gmail.com>
2024-12-03 17:32:07 +0300

    Add Explore and Users Follow Routes: include explore and user follow routes in the main router

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,11 +4,14 @@ import auth from "./auth/index.js";
 import post from "./post/index.js";
 import postLike from "./post/like.js";
 import comments from "./comments/index.js";
-import usersPost from "./users/post.js";
 import bookmarks from "./users/bookmarks.js";
 
-import search from "./search/index.js";
 import users from "./users/index.js";
+import usersPost from "./users/post.js";
+import usersFollow from "./users/follow.js";
+
+import search from "./search/index.js";
+import explore from "./explore/index.js";
 
 export default (app) => {
    const setRoutes = (routes) => {
@@ -17,5 +20,5 @@ export default (app) => {
          else app.use(prefix, route());
       });
    };
-   setRoutes([auth, post,postLike, users, usersPost, comments, search,bookmarks]);
+   setRoutes([auth, post, postLike, users, usersPost, usersFollow, comments, search, explore, bookmarks]);
 };
