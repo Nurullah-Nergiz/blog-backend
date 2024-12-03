ad4dd02e938fc3ec6198cebb5a405d515fa5edef
Nurullah NERGİZ <nurullah.devs@gmail.com>
2024-12-03 17:31:06 +0300

    Enhance Bookmarks Schema: add timestamps and disable versioning for bookmarks model

--- src/schema/bookmarks.js
+++ src/schema/bookmarks.js
@@ -1,6 +1,12 @@
-import { Schema, Types, model } from 'mongoose';
+import { Schema, Types, model } from "mongoose";
 
-export default model('Bookmarks',new Schema({
-    userId: { type: Types.ObjectId, ref: "User", required: true },
-    postId: { type: Types.ObjectId, ref: "Post", required: true },
-}));
+export default model(
+   "Bookmarks",
+   new Schema(
+      {
+         userId: { type: Types.ObjectId, ref: "User", required: true },
+         postId: { type: Types.ObjectId, ref: "Post", required: true },
+      },
+      { id: false, versionKey: false, timestamps: true }
+   )
+);
