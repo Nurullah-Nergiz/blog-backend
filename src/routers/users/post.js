96f36edbb46f5a2cdbff011576f69909f32be90d
Nurullah NERGİZ <nurullah.devs@gmail.com>
2024-12-23 17:16:45 +0300

    Update user posts retrieval: change route parameter to userName, enhance error handling for empty posts, and adjust response status codes

--- src/routers/users/post.js
+++ src/routers/users/post.js
@@ -1,22 +1,29 @@
 import { Router } from "express";
 import PostSchema from "../../schema/post.js";
+// import User from "../../models/user.js";
 // import {} from "../../validations/users.js";
 
 const route = () => {
    const app = new Router();
 
-   app.get("/:id/posts", (req, res) => {
-      PostSchema.find({ author: req.params.id })
+   app.get("/:userName/posts", (req, res) => {
+      const { userName } = req.params;
+
+      PostSchema.find()
          .populate([
             {
                path: "author",
+               match: { userName },
                model: "User",
             },
          ])
          .then((post) => {
+            if (post.length === 0) {
+               return res.status(404).json({ message: "Post not found" });
+            }
             res.json(post);
          })
-         .catch((err) => res.json(err));
+         .catch((err) => res.status(500).json(err));
    });
 
    return app;
@@ -24,6 +31,6 @@ const route = () => {
 
 export default {
    prefix: "/users/",
-   checkAuth: false,
+   isLogin: false,
    route,
 };
